ba3072e62c64a9c3dc7c34242d7cb8ee84492456
RagulKanna <ragulragi619@gmail.com>
2021-10-06 23:19:27 +0530

    Added:Calculating the employee wage for both part time and full time employee

--- Employeepayroll.js
+++ Employeepayroll.js
@@ -1,8 +1,19 @@
-const CHECK_EMPLOYEE_ABSENT = 0;
-let employee_check = Math.floor(Math.random() * 10) % 2;
-if (employee_check == CHECK_EMPLOYEE_ABSENT) {
-    console.log("Employee is absent...");
-    return;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let employeehours = 0;
+let employeecheck = Math.floor(Math.random() * 10) % 3;
+switch (employeecheck) {
+    case IS_PART_TIME:
+        employeehours = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        employeehours = FULL_TIME_HOURS;
+        break;
+    default:
+        employeehours = 0;
 }
-else
-    console.log("Employee is present...");
+let employeewage = employeehours * WAGE_PER_HOUR;
+console.log("Employee wage is " + employeewage);
